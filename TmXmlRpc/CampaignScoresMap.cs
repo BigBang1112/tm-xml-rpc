bb6a848b8281f27c14b266fba8e295ca8e3f8511
Petr Pivoňka <petrpiv1@gmail.com>
2021-07-25 04:52:02 +0200

    Add map equality

--- TmXmlRpc/CampaignScoresMap.cs
+++ TmXmlRpc/CampaignScoresMap.cs
@@ -12,5 +12,21 @@ namespace TmXmlRpc
         {
             return Zones.FirstOrDefault().Value.ToString();
         }
+
+        public bool Equals(CampaignScoresMap map)
+        {
+            if (map is null) return false;
+            return map.MapUid == MapUid;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as CampaignScoresMap);
+        }
+
+        public override int GetHashCode()
+        {
+            return MapUid?.GetHashCode() ?? base.GetHashCode();
+        }
     }
 }
