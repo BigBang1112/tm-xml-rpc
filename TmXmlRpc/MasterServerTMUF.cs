4a96269ab27829a09067d97b541a1dc2b20c2c30
Petr Pivoňka <petrpiv1@gmail.com>
2021-07-25 04:28:09 +0200

    Fix GetCampaignScoresUrl

--- TmXmlRpc/MasterServerTMUF.cs
+++ TmXmlRpc/MasterServerTMUF.cs
@@ -13,9 +13,16 @@ namespace TmXmlRpc
 
         }
 
-        public Uri GetCampaignScoresUrl(string campaign, int zoneId)
+        /// <summary>
+        /// Gets the URL where the leaderboard data is available.
+        /// </summary>
+        /// <param name="set">The set number. Can be 1 to 7.</param>
+        /// <param name="campaign">Official campaign name.</param>
+        /// <param name="zoneId">ID of the zone</param>
+        /// <returns>The URI of the leaderboard data download.</returns>
+        public static Uri GetCampaignScoresUrl(int set, string campaign, int zoneId)
         {
-            return new Uri($"https://scores.trackmaniaforever.com/scores4/{campaign}/{zoneId}{zoneId}.gz");
+            return new Uri($"https://scores.trackmaniaforever.com/scores{set}/{campaign}/{campaign}{zoneId}.gz");
         }
     }
 }
